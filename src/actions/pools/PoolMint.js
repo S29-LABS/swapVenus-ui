f3ae0d6ff07b4cbf8b301a941f1ca4bd573475e4
usihanselm <nots29vv@gmail.com>
2023-06-24 14:39:07 +0100

    third commit

--- src/actions/pools/PoolMint.js
+++ src/actions/pools/PoolMint.js
@@ -11,14 +11,14 @@ let CONTRACT_ADDRESS = "0x5c181549688c3dd4148d9e8bf4ebd0d3cc9de3c5";
 
 async function PoolMint() {
   
-        const config = await prepareWriteContract({
-          address: CONTRACT_ADDRESS,
-          abi: CONTRACT_ABI,
-          functionName: 'transferFrom',
-          args: ["0x2E0c954f186DC9ceDa9110dE30C9A83c77DA7E6a","0x84E4385EfEa4990A436b42E89D1a1a1BCFb3E018",1],
-        })
+        // const config = await prepareWriteContract({
+        //   address: CONTRACT_ADDRESS,
+        //   abi: CONTRACT_ABI,
+        //   functionName: 'transferFrom',
+        //   args: ["0x2E0c954f186DC9ceDa9110dE30C9A83c77DA7E6a","0x84E4385EfEa4990A436b42E89D1a1a1BCFb3E018",1],
+        // })
 
-        await writeContract(config)
+        // await writeContract(config)
 
         
 
