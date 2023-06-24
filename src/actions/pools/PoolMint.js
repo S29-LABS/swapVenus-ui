57567f4be2840e5b413c1ae0671a25ea0499ae67
usihanselm <nots29vv@gmail.com>
2023-06-24 14:45:37 +0100

    fifth commit

--- src/actions/pools/PoolMint.js
+++ src/actions/pools/PoolMint.js
@@ -1,6 +1,6 @@
-// import { prepareWriteContract, writeContract } from '@wagmi/core';
-// import CONTRACT_ABI  from "./chickenSoup.json";
-// let CONTRACT_ADDRESS = "0x5c181549688c3dd4148d9e8bf4ebd0d3cc9de3c5";
+import { prepareWriteContract, writeContract } from '@wagmi/core';
+import CONTRACT_ABI  from "./chickenSoup.json";
+let CONTRACT_ADDRESS = "0x5c181549688c3dd4148d9e8bf4ebd0d3cc9de3c5";
 
 
 
@@ -11,22 +11,21 @@
 
 async function PoolMint() {
   
-        // const config = await prepareWriteContract({
-        //   address: CONTRACT_ADDRESS,
-        //   abi: CONTRACT_ABI,
-        //   functionName: 'transferFrom',
-        //   args: ["0x2E0c954f186DC9ceDa9110dE30C9A83c77DA7E6a","0x84E4385EfEa4990A436b42E89D1a1a1BCFb3E018",1],
-        // })
+        const config = await prepareWriteContract({
+          address: CONTRACT_ADDRESS,
+          abi: CONTRACT_ABI,
+          functionName: 'mint',
+          args: [],
+        })
 
-        // await writeContract(config)
+        await writeContract(config)
 
         
 
         
   
 
-      // console.log(CONTRACT_ABI)
-      console.log("Hello World")
+     
 }
 
 export default PoolMint;
